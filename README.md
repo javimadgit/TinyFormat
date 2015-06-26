489daa76c90c154f21ec816c576b37e08dcd0be8
javiMaD <javimad@yandex.com>
2015-06-27 00:00:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## TinyFormat
+==========
+
+TinyFormat is a small utility to format Sys/EmuNAND using FS:InitializeCtrFileSystem but without formating your SD card.
+
+More info about FS:InitializeCtrFileSystem
+http://www.3dbrew.org/wiki/System_Settings (System Format)
+http://www.3dbrew.org/wiki/FS:InitializeCtrFileSystem
+
+## WARNING!
+## Use it at your own risk.
+## 3DS initial setup will delete MSET exploit.
+
+Download: https://github.com/javimadgit/TinyFormat/releases
+
+Requires [devkitARM](http://sourceforge.net/projects/devkitpro/files/devkitARM/) and [ctrcommon](https://github.com/Steveice10/ctrcommon) to build.
+
+Sound by BristolStories https://www.freesound.org/people/BristolStories/sounds/51710/
---
+++ source/main.c
@@ -0,0 +1,81 @@
+/*
+ * TinyFormat v1.0
+ * javiMaD
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <3ds.h>
+
+Result
+FS_InitializeCtrFileSystem(Handle handle)
+{
+	u32 *cmdbuf = getThreadCommandBuffer();
+
+	cmdbuf[0] = 0x08430000;
+
+	Result ret = 0;
+	if((ret = svcSendSyncRequest(handle)))
+		return ret;
+
+	ret = cmdbuf[1];
+	return ret;
+}
+
+int main(int argc, char** argv)
+{
+	// Initialize services
+	gfxInitDefault();
+	aptInit();
+	fsInit();
+
+	Result res;
+
+	// Init console for text output
+	consoleInit(GFX_BOTTOM, NULL);
+
+	printf("TinyFormat\n");
+	printf("----------\n\n");
+	printf("Press START to exit.\n");
+	printf("Press Y to start.\n\n");
+	
+	// Main loop
+	while (aptMainLoop())
+	{
+		hidScanInput();
+
+		u32 kDown = hidKeysDown();
+		if (kDown & KEY_START)
+		{
+			printf("CANCEL! Exiting...\n");
+			break;
+		}
+		else if (kDown & KEY_Y)
+		{
+			printf("InitializeCtrFileSystem. Please wait...\n");
+			res = FS_InitializeCtrFileSystem( *(fsGetSessionHandle()) );
+			if (res == 0)
+				printf("Done!\n");
+			else
+				printf("FAILED!\n");
+
+			printf("Wait for system reboot...\n");
+			svcSleepThread(3000000000);
+			aptOpenSession();
+			APT_HardwareResetAsync(NULL);
+			aptCloseSession();
+		}
+
+		// Flush and swap framebuffers
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+		gspWaitForVBlank();
+	}
+
+	// Exit services
+	fsExit();
+	aptExit();
+	gfxExit();
+	return 0;
+}
